0d207ffac0ca56e3aab48337c21dc02aa2598978
Leonardo-Calderaro <leonardocalderaroteixeira@gmail.com>
2022-07-14 12:09:40 -0300

    "Commit Update"

--- app/src/main/java/com/calderaro/MainActivity.kt
+++ app/src/main/java/com/calderaro/MainActivity.kt
@@ -2,10 +2,22 @@ package com.calderaro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.calderaro.databinding.ActivityMainBinding
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+//        setContentView(R.layout.activity_main)
+//        findViewById<TextView>(R.id.textViewHelloWorld)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.textViewHelloWorld.text = "Testando 1, 2, 3."
+
     }
 }
